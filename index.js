a1a06f4b8fc2c01f87706ab85fb15c80b528188e
Welton <webra.unb@gmail.com>
2019-07-30 12:59:01 -0300

    Update to work on tera-toolbox

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-module.exports = function AutoDailyRewardCredits(dispatch) {
-    dispatch.hook('S_LOGIN', 'raw', () => {
-        dispatch.toServer('C_REQUEST_RECV_DAILY_TOKEN', 1);
+module.exports = function AutoDailyRewardCredits(mod) {
+    mod.hook('S_LOGIN', 13, (event) => {
+        mod.toServer('C_REQUEST_RECV_DAILY_TOKEN', 1, {});
     })
 }
