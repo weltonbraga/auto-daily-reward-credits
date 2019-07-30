baf2c959f1897a003c69da9a74e2ab0adeaf3a9b
weltonbraga <webra.unb@gmail.com>
2019-07-30 14:42:36 -0300

    set auto update repository

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = function AutoDailyRewardCredits(mod) {
-    mod.hook('S_LOGIN', 13, (event) => {
+    mod.hook('S_LOGIN', '*', () => {
         mod.toServer('C_REQUEST_RECV_DAILY_TOKEN', 1, {});
     })
 }
